feb311ac9e93938ec569bc13a85ed2bc4854d4b4
ryunosuke ide <>
2018-12-20 01:36:03 +0900

    feat:ボックスの描画を別クラスに分ける

---
+++ TimelineEditor/Assets/Editor/BoxDrawer.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BoxDrawer : IDrawableParts {
+	static int    SelectedLineWidth = 2;
+	static Color  SelectedColor     = Color.blue;
+	
+	private bool    m_selected = false;
+	private Rect    m_rect;
+	private Texture m_texture = null;
+	private Color   m_color = Color.white;
+
+	public BoxDrawer()
+	{
+	}
+
+	public BoxDrawer(Rect rect)
+	{
+		m_selected = false;
+		m_rect = rect;
+	}
+
+	public void SetSelected(bool selected)
+	{
+		m_selected = selected;
+	}
+
+	public bool IsSelected()
+	{
+		return m_selected;
+	}
+
+	public void SetRect(Rect rect)
+	{
+		m_rect = rect;
+	}
+
+	public void SetSize(int w, int h)
+	{
+		m_rect.width = w;
+		m_rect.height = h;
+	}
+
+	public void SetPos(int x, int y)
+	{
+		m_rect.x = x;
+		m_rect.y = y;
+	}
+
+	public void SetColor(Color color)
+	{
+		m_color = color;
+	}
+
+	public Vector2 GetPos()
+	{
+		return m_rect.position;
+	}
+
+	public Rect GetRect()
+	{
+		return m_rect;
+	}
+
+	public Rect Draw()
+	{
+		if(m_selected) {
+			EditorGUI.DrawRect(
+				new Rect(
+					m_rect.x - SelectedLineWidth,
+					m_rect.y - SelectedLineWidth,
+					m_rect.width + SelectedLineWidth * 2,
+					m_rect.height + SelectedLineWidth * 2),
+				SelectedColor
+			);
+		}
+		EditorGUI.DrawRect(m_rect, m_color);
+
+		return m_rect;
+	}
+}
--- TimelineEditor/Assets/Editor/DraggableBox.cs
+++ TimelineEditor/Assets/Editor/DraggableBox.cs
@@ -4,83 +4,72 @@ using UnityEngine;
 using UnityEditor;
 
 public class DraggableBox : IDrawableParts, IDraggableParts {
-	Rect    m_rect;
-	Vector2 m_originPos   = Vector2.zero;
-	Color   m_color       = Color.red;
-	bool    m_selected    = false;
-	Vector2 m_mouseOffset = Vector2.zero;
+	private Vector2   m_originPos   = Vector2.zero;
+	private Vector2   m_mouseOffset = Vector2.zero;
+	private BoxDrawer m_boxDrawer   = new BoxDrawer();
 
 	public DraggableBox(Rect rect)
 	{
-		m_rect = rect;
-		m_originPos = new Vector2(m_rect.x, m_rect.y);
+		m_boxDrawer.SetRect(rect);
+		m_originPos = new Vector2(rect.x, rect.y);
 	}
 	public DraggableBox(Rect rect, Color color)
 	{
-		m_rect = rect;
-		m_originPos = new Vector2(m_rect.x, m_rect.y);
-		m_color = color;
+		m_boxDrawer.SetRect(rect);
+		m_boxDrawer.SetColor(color);
+		m_originPos = new Vector2(rect.x, rect.y);
 	}
 
 	public Rect Draw()
 	{
-		//GUI.Label(m_rect, "text");
-		//GUI.Box(m_rect, "testtext");
-		EditorGUI.DrawRect(m_rect, m_color);
-		//Debug.Log("@@@DrawRect " + m_rect);
-		return m_rect;
+		return m_boxDrawer.Draw();
 	}
 
 	private Vector2 Pos { 
 		get { 
-			return new Vector2(m_rect.x, m_rect.y); 
+			return m_boxDrawer.GetPos();
 		}
 		set {
-			m_rect.x = value.x;
-			m_rect.y = value.y;
+			m_boxDrawer.SetPos((int)value.x, (int)value.y);
 		} 
 	}
 
 	public void OnSelect(Vector2 mousePos)
 	{
-		m_selected = true;
+		m_boxDrawer.SetSelected(true);
 		m_mouseOffset = mousePos - Pos;
 		Debug.Log("@@@OnSelect");
 	}
 
 	public void OnUnselect()
 	{
-		m_selected = false;
+		m_boxDrawer.SetSelected(false);
 		Debug.Log("@@@OnUnselect");
 	}
 
 	public bool IsSelected()
 	{
-		return m_selected;
+		return m_boxDrawer.IsSelected();
 	}
 
 	public Rect GetRect()
 	{
-		return m_rect;
+		return m_boxDrawer.GetRect();
 	}
 
 	public void OnDrag(Vector2 move)
 	{
 		Pos = Pos + move;
-		//Debug.Log("@@@OnDrag m_rect:" + m_rect);
 	}
 
 	public void OnRelease()
 	{
-		m_originPos.x = m_rect.x;
-		m_originPos.y = m_rect.y;
-		Debug.Log("@@@OnRelease m_rect:" + m_rect);
+		m_originPos.x = m_boxDrawer.GetPos().x;
+		m_originPos.y = m_boxDrawer.GetPos().y;
 	}
 
 	public void OnRevert()
 	{
-		m_rect.x = m_originPos.x;
-		m_rect.y = m_originPos.y;
-		Debug.Log("@@@OnRevert m_rect:" + m_rect);
+		m_boxDrawer.SetPos((int)m_originPos.x, (int)m_originPos.y);
 	}
 }
