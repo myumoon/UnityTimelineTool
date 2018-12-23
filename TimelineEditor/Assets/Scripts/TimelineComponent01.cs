69a7befdd44658eeedfdb6278bdfa210847bd533
ryunosuke ide <ryunosukeide@ryunosukenoMacBook-puro.local>
2018-12-23 12:30:23 +0900

    chore:タイムラインテスト用のファイルを追加

---
+++ TimelineEditor/Assets/Scripts/TimelineComponent01.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CompoentData01 {
+	public int     m_intData;
+	public string  m_stringData;
+	public Vector3 m_vectorData;
+}
+
+public class TimelineComponent01 : MonoBehaviour {
+
+	[SerializeField]
+	private float m_start;
+
+	[SerializeField]
+	private float m_end;
+
+	[SerializeField]
+	private CompoentData01 m_componentData;
+}
