f837aa426e511089f1e3b707cde0202727a32f9b
ryunosuke ide <ryunosukeide@ryunosukenoMacBook-puro.local>
2019-01-12 14:07:57 +0900

    refactor:namespace追加

--- TimelineEditor/Assets/Editor/TimelineLayout/Parts/GUILayoutElementTraverser.cs
+++ TimelineEditor/Assets/Editor/TimelineLayout/Parts/GUILayoutElementTraverser.cs
@@ -2,50 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GUILayoutElementTraverser {
-	private GUILayoutNode m_root = null;
+namespace TimelineLayout {
+	
+	public class GUILayoutElementTraverser {
+		private GUILayoutNode m_root = null;
 
-	public GUILayoutElementTraverser(GUILayoutNode handledRoot)
-	{
-		m_root = handledRoot;
-	}
-
-	// 最後の子供を取得
-	public GUILayoutNode FindLastChild()
-	{
-		if(m_root == null) {
-			return null;
+		public GUILayoutElementTraverser(GUILayoutNode handledRoot)
+		{
+			m_root = handledRoot;
 		}
 
-		var element = m_root.childRoot;
-		if(element != null) {
-			while(element.nextSibling != null) {
-				element = element.nextSibling;
+		// 最後の子供を取得
+		public GUILayoutNode FindLastChild()
+		{
+			if(m_root == null) {
+				return null;
 			}
-		}
 
-		return element;
-	}
-
-	// 幅優先でトラバース
-	public IEnumerable<GUILayoutNode> Traverse()
-	{
-		Queue<GUILayoutNode> nodeQueue = new Queue<GUILayoutNode>();
-		if(m_root != null) {
-			yield return m_root;
-		}
-		nodeQueue.Enqueue(m_root);
-
-		GUILayoutNode execNode = null;
-		while(0 < nodeQueue.Count) {
-			execNode = nodeQueue.Dequeue();
-			yield return execNode;
-		
-			execNode = execNode.childRoot;
-			while(execNode != null) {
-				nodeQueue.Enqueue(execNode);
-				execNode = execNode.nextSibling;
+			var element = m_root.childRoot;
+			if(element != null) {
+				while(element.nextSibling != null) {
+					element = element.nextSibling;
+				}
 			}
+
+			return element;
+		}
+
+		// 幅優先でトラバース
+		public IEnumerable<GUILayoutNode> Traverse()
+		{
+			Queue<GUILayoutNode> nodeQueue = new Queue<GUILayoutNode>();
+			if(m_root != null) {
+				yield return m_root;
+			}
+			nodeQueue.Enqueue(m_root);
+
+			GUILayoutNode execNode = null;
+			while(0 < nodeQueue.Count) {
+				execNode = nodeQueue.Dequeue();
+				yield return execNode;
+			
+				execNode = execNode.childRoot;
+				while(execNode != null) {
+					nodeQueue.Enqueue(execNode);
+					execNode = execNode.nextSibling;
+				}
+			}
+
 		}
 
 	}
--- TimelineEditor/Assets/Editor/TimelineLayout/Parts/GUILayoutNode.cs
+++ TimelineEditor/Assets/Editor/TimelineLayout/Parts/GUILayoutNode.cs
@@ -2,72 +2,76 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GUILayoutNode {
-	private GUILayoutNode m_parent      = null;
-	private GUILayoutNode m_child       = null;
-	private GUILayoutNode m_prevSibling = null;
-	private GUILayoutNode m_nextSibling = null;
+namespace TimelineLayout {
+	
+	public class GUILayoutNode {
+		private GUILayoutNode m_parent      = null;
+		private GUILayoutNode m_child       = null;
+		private GUILayoutNode m_prevSibling = null;
+		private GUILayoutNode m_nextSibling = null;
 
-	// 親を取得
-	public GUILayoutNode parent {
-		private set { m_parent = value; }
-		get { return m_parent; }
-	}
-
-	// 最初の子を取得
-	public GUILayoutNode childRoot {
-		private set { m_child = value; }
-		get { return m_child; }
-	}
-
-	// 姉を取得
-	public GUILayoutNode prevSibling {
-		private set { m_prevSibling = value; }
-		get { return m_prevSibling; }
-	}
-
-	// 妹を取得
-	public GUILayoutNode nextSibling {
-		private set { m_nextSibling = value; }
-		get { return m_nextSibling; }
-	}
-
-	// 子を追加
-	public void AddChild(GUILayoutNode addedChild)
-	{
-		if(childRoot == null) {
-			childRoot = addedChild;
+		// 親を取得
+		public GUILayoutNode parent {
+			private set { m_parent = value; }
+			get { return m_parent; }
 		}
-		else {
-			GUILayoutElementTraverser traverser = new GUILayoutElementTraverser(childRoot);
+
+		// 最初の子を取得
+		public GUILayoutNode childRoot {
+			private set { m_child = value; }
+			get { return m_child; }
+		}
+
+		// 姉を取得
+		public GUILayoutNode prevSibling {
+			private set { m_prevSibling = value; }
+			get { return m_prevSibling; }
+		}
+
+		// 妹を取得
+		public GUILayoutNode nextSibling {
+			private set { m_nextSibling = value; }
+			get { return m_nextSibling; }
+		}
+
+		// 子を追加
+		public void AddChild(GUILayoutNode addedChild)
+		{
+			if(childRoot == null) {
+				childRoot = addedChild;
+			}
+			else {
+				GUILayoutElementTraverser traverser = new GUILayoutElementTraverser(childRoot);
+				var lastChild = traverser.FindLastChild();
+				lastChild.nextSibling = addedChild;
+			}
+		}
+
+		// 親にアタッチ
+		public void AttachToParent(GUILayoutNode targetParent)
+		{
+			GUILayoutElementTraverser traverser = new GUILayoutElementTraverser(targetParent);
 			var lastChild = traverser.FindLastChild();
-			lastChild.nextSibling = addedChild;
+
+			parent = targetParent;
+			lastChild.nextSibling = this;
+			prevSibling = lastChild;
+		}
+
+		// 親から切り離す
+		public void DetachFromParent()
+		{
+			if(parent.childRoot == this) {
+				parent.childRoot = nextSibling;
+			}
+			if(prevSibling != null) {
+				prevSibling.nextSibling = nextSibling;
+			}
+			if(nextSibling != null) {
+				nextSibling.prevSibling = prevSibling;
+			}
+			parent = null;
 		}
 	}
 
-	// 親にアタッチ
-	public void AttachToParent(GUILayoutNode targetParent)
-	{
-		GUILayoutElementTraverser traverser = new GUILayoutElementTraverser(targetParent);
-		var lastChild = traverser.FindLastChild();
-
-		parent = targetParent;
-		lastChild.nextSibling = this;
-		prevSibling = lastChild;
-	}
-
-	// 親から切り離す
-	public void DetachFromParent()
-	{
-		if(parent.childRoot == this) {
-			parent.childRoot = nextSibling;
-		}
-		if(prevSibling != null) {
-			prevSibling.nextSibling = nextSibling;
-		}
-		if(nextSibling != null) {
-			nextSibling.prevSibling = prevSibling;
-		}
-		parent = null;
-	}
 }
