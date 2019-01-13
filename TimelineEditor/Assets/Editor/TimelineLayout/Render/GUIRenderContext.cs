bec30c32b7490c8f0020b67130a7506beb547123
ryunosuke_ide <ryumyu0120@gmail.com>
2019-01-13 19:32:51 +0900

    chore:フォルダ階層を整理

---
+++ TimelineEditor/Assets/Editor/TimelineLayout/Render/GUIRenderContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+// 描画コンテキスト
+public class GUILayoutRenderContext {
+	public Texture texture { get; set; }
+	public Rect    rect { get; set; }
+}
